894b9927c4af67d86b85b2d2010b4683a4aa0b79
Anas <anas.wanaw@gmail.com>
2018-08-23 20:07:41 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials'); //__dirname stocks the path of parent directory
+app.set('view engine','hbs'); //will tell what view engine we wanna use ? HBS in this case
+
+app.use((req,res,next) => {
+        var now = new Date().toString();
+        var log = `${now} : ${req.method}  ${req.path} \n`;
+        fs.appendFile('server.log',log,(err) => {
+                if(err) console.log('unable to append to server.log');
+        });
+        console.log(`${now} ${req.method}  ${req.path}`);
+        next();
+});
+
+/*
+app.use((req,res,next) => {
+                res.render('maintenance.hbs');
+});
+MAINTENANCE PAUSE
+*/
+app.use(express.static(__dirname + '/public')); //for static front-end pages
+
+hbs.registerHelper('getCurrentYear', () => {
+        return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+        return text.toUpperCase();
+});
+
+app.get('/',(req,res) => { 
+        res.render('home.hbs',{
+                pageTitle : 'Home Page',
+                welcomeMessage : 'Welcome buddy!',  
+        })
+});
+
+app.get('/about',(req,res) => {
+        res.render('about.hbs',{
+                pageTitle : 'About page',
+        });
+});
+
+app.get('/bad',(req,res) => {
+        res.send({
+                errorMessage : "I don't know where u're goin' man.."
+        })
+});
+
+//app.listen(3000);
+app.listen(3000,() =>{
+        console.log('server is up on port 3000'); 
+});
